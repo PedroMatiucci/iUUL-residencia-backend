aa3fee7098d80e8edcbd42592df3833e1358e2f2
Rodrigo C. Arruda <rcbsa@protonmail.com>
2022-12-30 18:23:30 -0300

    Pequena correção mensagem View
    AGORA CHEGA

--- Desafio1/Desafio1/View/ViewMensagens.cs
+++ Desafio1/Desafio1/View/ViewMensagens.cs
@@ -31,7 +31,7 @@ namespace Consultorio.View
         internal static void ExibeMensagemAgendamento(bool v)
         {
             if(v) Console.WriteLine("\nAgendamento realizado com sucesso");
-            else Console.WriteLine("\nErro: paciente já possui uma consulta futura agendada.");
+            else Console.WriteLine("\nErro: horário indisponível ou paciente já possui consulta futura agendada.");
         }
         internal static void ExibeMensagemErroHorarioComercial()
         {
